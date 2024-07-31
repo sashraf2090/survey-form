ca23b3215c5102c1889153d3498f001b2f4b0499
sashraf2090 <sashraf2090@gmail.com>
2024-07-31 00:22:19 -0400

    Update SurveyFormApplication.java

--- src/main/java/com/example/survey_form/SurveyFormApplication.java
+++ src/main/java/com/example/survey_form/SurveyFormApplication.java
@@ -1,7 +1,7 @@
 package com.example.survey_form;
 
 // Author: Sadaf Ashraf
-// SurveyFormApplication Main Class
+// SurveyFormApplication Main Class for Survey Form
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
